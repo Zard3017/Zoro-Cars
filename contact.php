a1d696af63704e57aa4c8eb43179c14e4f79e6c3
Zard3017 <ianbradley3017@gmail.com>
2024-06-17 21:51:05 +0300

    changed contact to php

---
+++ contact.php
@@ -0,0 +1,77 @@
+<?php 
+include_once ("template/nav.php");
+include_once("template/links.php"); 
+    
+?>
+  
+   
+
+    <section class="contact">
+        <div class="content">
+            <h2>Have a question? We'd love to hear from you!</h2>
+            <p>Fill out the contact form below or reach out to us via phone or email.  We typically respond to 
+                inquiries within 24 hours.</p>
+        </div>
+
+        <div class="contactcontainer">
+            <div class="contactInfo">
+
+                <div class="box">
+                 <div class="icon"></div>
+                    <div class="text">
+                        <h3>Address</h3>
+                        <p>24th Street Campana,<br> Nairobi, Kenya,<br>11230</p>
+                    </div>
+                </div>
+                <div class="contactbox">
+                  <div class="icon">
+                    
+                  </div>
+                    <div class="text">
+                    <h3>Phone</h3>
+                    <p>+254718790446</p>
+                
+                </div>
+            </div>
+         
+         <div class="contactbox">
+            <div class="icon"></i></div>
+            <div class="text">
+            <h3>Email</h3>
+            <p>aldaircooperation@gmail.com</p>
+        
+        </div>
+    </div>
+  
+ <div class="contactForm">
+    <form action="">
+        <h2>Send us a message</h2>
+        <div class="inputBox">
+            <input type="text" name="" required="required">
+            <span>Full Name</span>
+        </div>
+        <div class="inputBox">
+            <input type="text" name="" required="required">
+            <span>Email</span>
+        </div>
+        <div class="inputBox">
+            <textarea name="" required="required" id=""></textarea>
+            <span>Type your message...</span>
+        </div>
+        <div class="inputBox">
+            <input type="submit" name=""  value="Send">
+            
+        </div>
+
+    </form>
+ </div>
+     </div>
+
+    </section>
+    
+
+    
+
+    
+
+    
--- index.php
+++ index.php
@@ -2,14 +2,10 @@
 include_once ("template/nav.php");
 include_once("template/links.php"); 
     
-    
-   
-
-    
 ?>
   
  
-
+             <title>Aldair</title>
              <div class="wrapper">
               <header style="background-color: white;">
                 <h1>Why Choose Us?</h1>
@@ -44,3 +40,4 @@ include_once("template/links.php");
              <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
              <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.min.js" integrity="sha384-0pUGZvbkm6XF6gxjEnlmuGrJXVbNuzT9qBBavbLwCsOGabYfZo0T0to5eqruptLy" crossorigin="anonymous"></script>
              
+             <?php?>
