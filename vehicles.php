dc839277c372ebf917d6a76a3d77cba47e6db044
Zard3017 <ianbradley3017@gmail.com>
2024-06-18 13:04:47 +0300

    added php

--- vehicles.php
+++ vehicles.php
@@ -1,15 +1,13 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Vehicles|Aldair</title>
-    <link rel="icon" href="images/Nuestro Nuevo Logo !.jpeg">
-    <link rel="stylesheet" href="CSS/style.css">
+<?php 
+include_once ("template/nav.php");
+include_once("template/links.php"); 
+   
+
+?>
+
     <link rel="stylesheet" href="CSS/vehicle.css">
-</head>
-<body >
-    <header>
+
+    <!--<header>
         <div class="logo"><a href="#">Aldair</a>
         </div>
         <div class="menu">
@@ -37,6 +35,7 @@
         </div>
        
     </header>
+-->
 
     <div class="container">
         <h1>Vehicles Available in stock</h1>
@@ -93,5 +92,3 @@
    
 
 
-</body>
-</html>
