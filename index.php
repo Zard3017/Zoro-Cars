8d5814305fbc39843dd04516033f166abf11b3dc
Zard3017 <ianbradley3017@gmail.com>
2024-06-24 10:01:43 +0300

    added footer to index

--- index.php
+++ index.php
@@ -1,6 +1,7 @@
 <?php 
 include_once ("template/nav.php");
 include_once("template/links.php"); 
+
    
 
 ?>
@@ -42,4 +43,6 @@ include_once("template/links.php");
              <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
              <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.min.js" integrity="sha384-0pUGZvbkm6XF6gxjEnlmuGrJXVbNuzT9qBBavbLwCsOGabYfZo0T0to5eqruptLy" crossorigin="anonymous"></script>
              
-             <?php?>
+             <?php
+             include_once("template/footer.php")
+             ?>
---
+++ template/footer.php
@@ -0,0 +1,44 @@
+<footer>
+  <div class="container">
+    <div class="row">
+      <div class="col-md-3">
+        <h4>About Us</h4>
+        <ul>
+          <li><a href="#">Our Story</a></li>
+          <li><a href="#">Our Team</a></li>
+          <li><a href="#">Careers</a></li>
+        </ul>
+      </div>
+      <div class="col-md-3">
+        <h4>Buy a Car</h4>
+        <ul>
+          <li><a href="#">Search Inventory</a></li>
+          <li><a href="#">Get a Quote</a></li>
+          <li><a href="#">Financing Options</a></li>
+        </ul>
+      </div>
+      <div class="col-md-3">
+        <h4>Sell a Car</h4>
+        <ul>
+          <li><a href="#">Get an Offer</a></li>
+          <li><a href="#">Trade-In Program</a></li>
+          <li><a href="#">Sell to Us</a></li>
+        </ul>
+      </div>
+      <div class="col-md-3">
+        <h4>Resources</h4>
+        <ul>
+          <li><a href="#">FAQs</a></li>
+          <li><a href="#">Blog</a></li>
+          <li><a href="#">Contact Us</a></li>
+        </ul>
+      </div>
+    </div>
+    <div class="row">
+      <div class="col-md-12">
+        <p>&copy; 2024 Aldair. All Rights Reserved.</p>
+        
+      </div>
+    </div>
+  </div>
+</footer>
