951610e00e23f00d73561b3bfcaaeb6a43c443a8
Zard3017 <ianbradley3017@gmail.com>
2024-07-01 14:49:24 +0300

    added view data.php

--- template/nav.php
+++ template/nav.php
@@ -10,7 +10,15 @@
         <ul>
             <li><a  href="index.php" class="under">Home</a>  </li>
             <li><a   href="sustainability.php" class="under">Sustainability</a>  </li>
-            <li><a   href="vehicles.php"  class="under">Vehicles</a>  </li>             
+            <li><a   href="vehicles.php"  class="under">Vehicles</a>  </li>
+            <div class="dropdown">
+  <button class="dropbtn">Vehicles</button>
+  <div class="dropdown-content">
+    <a href="#">New Cars</a>
+    <a href="#">Used cars</a>
+    <a href="#">Link 3</a>
+  </div>
+</div>  
             <li><a   href="articles.php" class="under">Articles</a>  </li>
             <li><a    href="FAQs.php" class="under">FAQ</a></li>
             <li><a   href="contact.php" class="under">Contact Us</a></li>  
