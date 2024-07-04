08f17c918365af23ec0e17801c2e060a7e15f940
Zard3017 <ianbradley3017@gmail.com>
2024-07-04 10:53:24 +0300

    added user button

--- displaydata.php
+++ displaydata.php
@@ -1,3 +1,18 @@
 <?php
 include_once("template/nav.php");
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Display Data</title>
+
+</head>
+<body>
+    <div class="container">
+        <button>Add User</button>
+    </div>
+</body>
+</html>
